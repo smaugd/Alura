ecd7f50e7481275ba493809217d656d45538fb85
smaugd <cainhotnt@gmail.com>
2023-12-20 14:02:23 -0300

    adicionando novos projetos base

---
+++ alugames/js/app.js
@@ -0,0 +1,3 @@
+// Você precisará examinar a estrutura do HTML, entender qual função está sendo chamada, e criar no arquivo app.js (já importado na página) todo o algoritmo para fazer o efeito. Ao clicar no jogo, deverá verificar se está alugado, trocar o texto do botão e mudar o visual do elemento.
+
+function alterarstatus(id) {}
